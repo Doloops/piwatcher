7706edc0759ebf253b209ee83d20d24b02376ee2
Francois Barre <francois.barre@arondor.com>
2017-10-07 23:29:13 +0200

    Misc bugfixing, convert to std init.d layouts

--- piwatcher-daemon.sh
+++ piwatcher-daemon.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
-# Mostly generic Daemon
-# Sample script taken from http://werxltd.com/wp/2012/01/05/simple-init-d-script-template (Thanks !)
+# Mostly generic Daemon for PIWatcher
 #
-# chkconfig: 345 20 80
+### BEGIN INIT INFO
+# Provides:          piwatcher
+# Required-Start:    $remote_fs $syslog $time
+# Required-Stop:     $remote_fs $syslog $time
+# Should-Start:      $network $named slapd autofs ypbind nscd nslcd winbind
+# Should-Stop:       $network $named slapd autofs ypbind nscd nslcd winbind
+# Default-Start:     2 3 4 5
+# Default-Stop:
+# Short-Description: PIWatcher monitors cpu, temps and disk activity
+# Description:       PIWatcher monitors cpu, temps and disk activity
+### END INIT INFO
+
+
 # description: PIWatcher Daemon
 # processname: piwatcher
+# Sample script taken from http://werxltd.com/wp/2012/01/05/simple-init-d-script-template (Thanks !)
 
 . /etc/piwatcher-daemon.conf
 
@@ -18,8 +30,8 @@ start)
 	if [ -n $DAEMON_PATH ]; then
     	cd $DAEMON_PATH
     fi
-	PID=`$DAEMON_EXEC $DAEMONOPTS > $LOGFILE 2>&1 & echo $!`
-	echo "Saving PID" $PID " to " $PIDFILE
+	PID=`$DAEMON_EXEC $DAEMON_OPTS > $LOGFILE 2>&1 & echo $!`
+	# echo "Saving PID" $PID " to " $PIDFILE
     if [ -z $PID ]; then
         printf "%s\n" "Fail"
     else
