55dbe47ef59677b0be603495fcc2884b5b21d482
Francois Barre <francois.barre@arondor.com>
2017-10-25 10:46:10 +0200

    Add cmdline testing for picommandwatcher Support PWM in picommandwatcher as well

--- picommandwatcher.py
+++ picommandwatcher.py
@@ -1,11 +1,12 @@
+import piwatcherconfig
 import pimodule
 import elasticsearch
 import time
+import sys
 import RPi.GPIO as GPIO
 from datetime import datetime
 
 class PiCommandWatcher(pimodule.PiModule):
-
     es = None
     hostname = None
     esIndex = None
@@ -17,6 +18,7 @@ class PiCommandWatcher(pimodule.PiModule):
     commands = None
     lastESUpdate = time.time()
     statsInterval = 60
+    pwms = {}
 
     def __init__(self, hosts, hostname, esIndex, esType, esId, esPropertyName, esPropertyDefaultValue, channels, commands):
         pimodule.PiModule.__init__(self,"PiCommandWatcher")
@@ -33,9 +35,19 @@ class PiCommandWatcher(pimodule.PiModule):
         self.commands = commands
         for name in self.channels:
             channel = self.channels[name]
-            pinout = channel["pinout"]
-            print("Configuring channel " + name + " to pinout " + str(pinout))
-            GPIO.setup(pinout, GPIO.OUT)
+            if "pinout" in channel:
+                pinout = channel["pinout"]
+                print("Configuring channel " + name + " to pinout " + str(pinout))
+                GPIO.setup(pinout, GPIO.OUT)
+                GPIO.output(pinout, False)
+            elif "pwm" in channel:
+                pinout = channel["pwm"]
+                freq = channel["freq"]
+                print("Configuring channel " + name + " to pwm " + str(pinout) + ", freq=" + str(freq))
+                GPIO.setup(pinout, GPIO.OUT)                
+                pwm = GPIO.PWM(pinout, freq)
+                pwm.start(0)
+                self.pwms[name] = pwm
 
     def update(self, measure):
         timestamp = datetime.utcnow()
@@ -75,15 +87,48 @@ class PiCommandWatcher(pimodule.PiModule):
                 commandValue = command[commandKey]
                 if commandKey in self.channels:
                     channel = self.channels[commandKey]
-                    pinout = channel["pinout"]
+                    if "pinout" in channel:
+                        pinout = channel["pinout"]
+                        GPIO.output(pinout, commandValue)
+                    elif "pwm" in channel:
+                        if isinstance(commandValue, bool):
+                            if commandValue:
+                                commandValue = 50
+                            else:
+                                commandValue = 0
+                        self.pwms[commandKey].ChangeDutyCycle(commandValue)
 #                    print(", {channel:" + commandKey + ", pin:" + str(pinout) + "=" + str(commandValue) + "}", end='')
-                    GPIO.output(pinout, commandValue)
                 else:
                     raise ValueError("Invalid command key " + commandKey)
         else:
-            raise ValueError("Unknown value " + propertyValue + " for property " + propertyName)
+            raise ValueError("Unknown value '" + propertyValue + "' for property " + propertyName + ", commands=" + str(self.commands.keys()))
 
     def shutdown(self):
         print("Shutdown " + self.getModuleName())    
         GPIO.cleanup()
+
+
+if __name__=="__main__":
+    pwConfig = piwatcherconfig.PiWatcherConfig.getConfig()
+    if "picommander" in pwConfig:    
+        picmd = PiCommandWatcher(
+            hosts = pwConfig["picommander"]["hosts"], 
+            hostname = pwConfig["hostname"],
+            esIndex = pwConfig["picommander"]["index"], 
+            esType = pwConfig["picommander"]["type"],
+            esId = pwConfig["picommander"]["id"],
+            esPropertyName = pwConfig["picommander"]["property"],
+            esPropertyDefaultValue = pwConfig["picommander"]["defaultValue"],        
+            channels = pwConfig["picommander"]["channels"],
+            commands = pwConfig["picommander"]["commands"]
+            )
+    while True:
+        print("Select command:")
+        input = sys.stdin.readline().strip()
+        print("Read : " + input)
+        try:
+            picmd.applyCommand("input", input)
+        except Exception as err:
+            print(" ! Caught " + str(err))
+            print("Invalid input : " + input)
 
