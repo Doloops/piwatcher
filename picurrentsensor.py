0b97ca754d054241da75f33ec72432a3a4023c4b
Francois Barre <francois.barre@arondor.com>
2017-11-15 21:54:53 +0100

    Also fix lf

--- picurrentsensor.py
+++ picurrentsensor.py
@@ -129,7 +129,7 @@ class PiCurrentSensor(pimodule.PiModule):
                 gap, wPrim = self.readAmplifiedChannel(channel, measure[sensorId])
             else:
                 print("! Unsupported type : " + type + " for sensorId=" + sensorId)
-            print(", #" + sensorId + "=" + ("%.2f" % wPrim) + "W (" + ("%.3f" % gap) + ")")            
+            print(", #" + sensorId + "=" + ("%.2f" % wPrim) + "W (" + ("%.3f" % gap) + ")", end='')            
 
 if __name__ == "__main__":
     pics = PiCurrentSensor({})
