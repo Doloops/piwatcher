cdc05fd023404eef9d26f72fe257768ab8fe41c4
Francois Barre <francois.barre@arondor.com>
2018-01-20 11:06:42 +0100

    Fix redeploy script

--- redeploy_piwatcher.sh
+++ redeploy_piwatcher.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-HOSTS="osmc@osmc pi@pizero1 pi@pizero2 pi@pizero3"
-# HOSTS="pi@pizero1"
+ALL_HOSTS="osmc@osmc pi@pizero1 pi@pizero2 pi@pizero3 pi@banane"
 
+# To allow sudo without password, add this to /etc/sudoers
+# pi	ALL=NOPASSWD: /etc/init.d/piwatcher-daemon
+
+HOSTS=$1
+if [ -z $HOSTS ] ; then
+    HOSTS="$ALL_HOSTS"
+fi
+echo "Running on HOSTS=$HOSTS"
 for HOST in $HOSTS ; do
     echo "HOST $HOST"
 cat << EOF | ssh -T $HOST
